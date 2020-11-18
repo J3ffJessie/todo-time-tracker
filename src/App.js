ca72380c4e30c940b1c1be5422ca8c52ab1fcfc7
j3ffjessie <jejess6125@gmail.com>
2020-11-18 14:43:20 -0600

    Add packages (Formik) for forms, Yup for form validation. Form Styled form.css and inputs created. Logs key: value pairs to console.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
-import MyForm from './Components/form';
+import GoalForm from './Components/form';
 import Table from './Components/table';
 
 function App() {
   return (
     <div className="App">
       <h1>ToDo/Goal Tracker</h1>
-      <MyForm />  
-      <Table />
+      <GoalForm />
     </div>
-  );
-}
+  )
+};
 
 export default App;
